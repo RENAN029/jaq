2b37042cf037f52d8ceb33d8ea3ef4fc833df724
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-29 09:59:33 -0300

    Update c.sh

--- c.sh
+++ c.sh
@@ -3,7 +3,7 @@ set -e
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
 sudo pacman -S --noconfirm gdu nvidia-open intel-ucode git neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom
-echo "sudo pacman -S iwd nix ananicy-cpp clamav irqbalance cups pnpm mise mangohud distrobox docker gamescope tailscale" 
+echo "sudo pacman -S" 
 sudo pacman -S --noconfirm cosmic-session cosmic-terminal cosmic-files cosmic-store xdg-user-dirs 
  
 sudo pacman -S --noconfirm gamemode fuse dnsmasq apparmor 
